9cf428d267e386473a433183e56f73d822e3a917
Arpon Raksit <arpon.raksit@gmail.com>
2014-01-08 02:50:45 -0500

    switched to websocket

---
+++ static/j.js
@@ -0,0 +1,41 @@
+var input, output, websocket;
+
+function showMessage(m) {
+    var p = document.createElement("p");
+    p.innerHTML = m;
+    output.appendChild(p);
+}
+
+function onMessage(e) {
+    showMessage(e.data);
+}
+
+function onClose() {
+    showMessage("Connection closed.");
+}
+
+function sendMessage() {
+    var m = input.value;
+    input.value = "";
+    websocket.send(m);
+    showMessage(m);
+}
+
+function onKey(e) {
+    if (e.keyCode == 13) {
+        sendMessage();
+    }
+}
+
+function init() {
+    input = document.getElementById("input");
+    input.addEventListener("keyup", onKey, false);
+
+    output = document.getElementById("output");
+
+    websocket = new WebSocket("ws://localhost:4000/socket");
+    websocket.onmessage = onMessage;
+    websocket.onclose = onClose;
+}
+
+window.addEventListener("load", init, false);
--- talk.go
+++ talk.go
@@ -2,18 +2,57 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io"
 	"log"
-	"net"
+	"net/http"
+
+	"code.google.com/p/go.net/websocket"
 )
 
-const addr = "localhost:8080"
+const addr = "localhost:4000"
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	http.Handle("/socket", websocket.Handler(socketHandler))
+	http.Handle("/static/", http.FileServer(http.Dir("./")))
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+var rootTemplate = template.Must(template.ParseFiles("root.html"))
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	rootTemplate.Execute(w, addr)
+}
+
+type socket struct {
+	io.ReadWriter
+	done chan bool
+}
+
+func (s socket) Close() error {
+	s.done <- true
+	return nil
+}
+
+func socketHandler(ws *websocket.Conn) {
+	s := socket{ws, make(chan bool)}
+	go match(s)
+	<-s.done
+}
 
 var partner = make(chan io.ReadWriteCloser)
 
-func send(t io.Writer, f io.Reader, errch chan<- error) {
-	_, err := io.Copy(t, f)
-	errch <- err
+func match(u io.ReadWriteCloser) {
+	fmt.Fprint(u, "Waiting for a partner...")
+	select {
+	case partner <- u:
+	case v := <-partner:
+		talk(u, v)
+	}
 }
 
 func talk(u, v io.ReadWriteCloser) {
@@ -29,25 +68,7 @@ func talk(u, v io.ReadWriteCloser) {
 	v.Close()
 }
 
-func match(u io.ReadWriteCloser) {
-	fmt.Fprint(u, "Waiting for a partner...")
-	select {
-	case partner <- u:
-	case v := <-partner:
-		talk(u, v)
-	}
-}
-
-func main() {
-	l, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for {
-		u, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		go match(u)
-	}
+func send(t io.Writer, f io.Reader, errch chan<- error) {
+	_, err := io.Copy(t, f)
+	errch <- err
 }
