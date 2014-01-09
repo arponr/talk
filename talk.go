1e02379d0bd84465685407ca55a9abc9ac53fe0c
Arpon Raksit <arpon.raksit@gmail.com>
2014-01-09 00:12:45 -0500

    ok port thing should be correct now...

--- talk.go
+++ talk.go
@@ -6,7 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
-	// "os"
+	"os"
 
 	"code.google.com/p/go.net/websocket"
 )
@@ -15,7 +15,7 @@ func main() {
 	http.HandleFunc("/", rootHandler)
 	http.Handle("/socket", websocket.Handler(socketHandler))
 	http.Handle("/static/", http.FileServer(http.Dir(".")))
-	err := http.ListenAndServe("localhost:4000", nil)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
